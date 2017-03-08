99b90e2dc33b66fb7fb588b49505071d35918bc4
duongvu96 <tvunguyen@wesleyan.edu>
2017-03-07 19:43:02 -0500

    first commit

---
+++ code_1.js
@@ -0,0 +1,12 @@
+var startBtn;
+var stopBtn;
+var popAudio;
+
+var running;
+var interval;
+
+startBtn.addEventListener("click", function() {
+});
+
+stopBtn.addEventListener("click", function() {
+});
